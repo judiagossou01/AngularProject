62f28cf1dca120f3e38c7be3eb1e9eddf0ba7d36
Judicaël Agossou <judi.agossou01@gmail.com>
2024-10-24 12:42:07 +0200

    Init angular files

---
+++ src/app/pages/register/register.component.ts
@@ -0,0 +1,24 @@
+import {Component, inject} from '@angular/core';
+import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+  fb = inject(FormBuilder);
+
+  form = this.fb.nonNullable.group({
+    name: ['', Validators.required],
+    email: ['', Validators.required],
+    password: ['', Validators.required]
+  });
+
+  submit(event: Event) {
+    event.preventDefault();
+    console.log(this.form.value);
+  }
+}
